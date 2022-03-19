930c8a86dd87b290f77e8b428059f283e495a7f1
PDob02 <patrickdobry@gmail.com>
2022-03-18 20:55:39 -0500

    Adding to Mapping Lines

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,24 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+// // Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+    color: "yellow"
+ }).addTo(map);
 
 // L.circle([34.0522, -118.2437], {
 //     radius: 100
@@ -35,7 +51,7 @@ cityData.forEach(function(city) {
 //   });
 
 // // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 5,
     id: 'mapbox/streets-v11',
