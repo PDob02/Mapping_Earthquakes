03b04f9ba066c13c8094071e452494d9074e60f8
PDob02 <patrickdobry@gmail.com>
2022-03-19 18:28:04 -0500

    After Logic Step 5 completion

--- static/js/logicStep5.js
+++ static/js/logicStep5.js
@@ -118,36 +118,36 @@ function getRadius(magnitude) {
 //       }).addTo(map);
 //   });
 
+// // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data, {
+//   // We turn each feature into a circleMarker on the map.
+//     pointToLayer: function(feature, latlng) {
+//         console.log(data);
+//         return L.circleMarker(latlng);
+//       },
+//   // We set the style for each circleMarker using our styleInfo function.
+//   style: styleInfo,
+//   // We create a popup for each circleMarker to display the magnitude and
+//   //  location of the earthquake after the marker has been created and styled.
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+//     }
+//   }).addTo(earthquakes);
+
+//     // earthquakes.addTo(map);
+
+// });
+
+
 // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-  // We turn each feature into a circleMarker on the map.
-    pointToLayer: function(feature, latlng) {
-        console.log(data);
-        return L.circleMarker(latlng);
-      },
-  // We set the style for each circleMarker using our styleInfo function.
-  style: styleInfo,
-  // We create a popup for each circleMarker to display the magnitude and
-  //  location of the earthquake after the marker has been created and styled.
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-    }
-  }).addTo(earthquakes);
-
-    // earthquakes.addTo(map);
-
-});
-
-
-// Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
+L.geoJSON(data, {
     // We turn each feature into a circleMarker on the map.
       pointToLayer: function(feature, latlng) {
           console.log(data);
           return L.circleMarker(latlng);
         },
     // We set the style for each circleMarker using our styleInfo function.
-    style: styleInfo,
+      style: styleInfo,
     // We create a popup for each circleMarker to display the magnitude and
     //  location of the earthquake after the marker has been created and styled.
       onEachFeature: function(feature, layer) {
@@ -157,37 +157,36 @@ function getRadius(magnitude) {
 
       earthquakes.addTo(map);
       // Create a legend control object.
-  let legend = L.control({
-    position: "bottomright"
+    let legend = L.control({
+      position: "bottomright"
   });
 
   // Then add all the details for the legend.
-    legend.onAdd = function() {
+    legend.onAdd = function(map) {
       let div = L.DomUtil.create("div", "info legend");
-    
-    const magnitudes = [0, 1, 2, 3, 4, 5];
-    const colors = [
-      "#98ee00",
-      "#d4ee00",
-      "#eecc00",
-      "#ee9c00",
-      "#ea822c",
-      "#ea2c2c"
-    ];
+
+      const magnitudes = [0, 1, 2, 3, 4, 5];
+      const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+      ];
 
   // Looping through our intervals to generate a label with a colored square for each interval.
-    for (var i = 0; i < magnitudes.length; i++) {
-      console.log(colors[i]);
-      div.innerHTML +=
-        "<i style='background: " + colors[i] + "'></i> " +
-        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-      }
-        return div;
-    };
-    
-  legend.addTo(map);
+      for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+          "<i style='background: " + colors[i] + "'></i> " +
+          magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+      };
 
-
+    return div;
+  };
+    legend.addTo(map);
+});
 // // Grabbing our GeoJSON data.
 // d3.json(torontoHoods).then(function(data) {
 //   console.log(data);
