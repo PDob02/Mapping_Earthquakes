fdf22dd45d06481877eb419ce3d6c27f2a650c73
PDob02 <patrickdobry@gmail.com>
2022-03-20 17:45:45 -0500

    cleaning up code

--- static/js/challenge_logic_3.js
+++ static/js/challenge_logic_3.js
@@ -107,8 +107,9 @@ L.geoJSON(data, {
   }
 }).addTo(majorEarthquakes);
 
-  majorEarthquakes.addTo(map);
   });
+  majorEarthquakes.addTo(map);
+ 
 
 
 // Retrieve the earthquake GeoJSON data.
@@ -124,69 +125,6 @@ function styleInfo(feature) {
     weight: 0.5
   };
 }
-
-// // This function determines the color of the circle based on the magnitude of the earthquake.
-// function getColor(magnitude) {
-//   if (magnitude > 5) {
-//     return "#ea2c2c";
-//   }
-//   if (magnitude > 4) {
-//     return "#ea822c";
-//   }
-//   if (magnitude > 3) {
-//     return "#ee9c00";
-//   }
-//   if (magnitude > 2) {
-//     return "#eecc00";
-//   }
-//   if (magnitude > 1) {
-//     return "#d4ee00";
-//   }
-//   return "#98ee00";
-// }
-// // This function determines the radius of the earthquake marker based on its magnitude.
-// // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
-// function getRadius(magnitude) {
-//   if (magnitude === 0) {
-//     return 1;
-//   }
-//   return magnitude * 4;
-// }
-
-// L.geoJSON(data, {
-
-// //   // We turn each feature into a circleMarker on the map.
-  
-//   pointToLayer: function(feature, latlng) {
-//               console.log(data);
-//               return L.circleMarker(latlng);
-//           },
-//         // We set the style for each circleMarker using our styleInfo function.
-//       style: styleInfo
-//       }).addTo(map);
-//   });
-
-// // Creating a GeoJSON layer with the retrieved data.
-//   L.geoJSON(data, {
-//   // We turn each feature into a circleMarker on the map.
-//     pointToLayer: function(feature, latlng) {
-//         console.log(data);
-//         return L.circleMarker(latlng);
-//       },
-//   // We set the style for each circleMarker using our styleInfo function.
-//   style: styleInfo,
-//   // We create a popup for each circleMarker to display the magnitude and
-//   //  location of the earthquake after the marker has been created and styled.
-//     onEachFeature: function(feature, layer) {
-//       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-//     }
-//   }).addTo(earthquakes);
-
-//     // earthquakes.addTo(map);
-
-// });
-
-
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data, {
     // We turn each feature into a circleMarker on the map.
@@ -254,30 +192,3 @@ L.geoJSON(data, {
 })
 .addTo(tectonicPlates);
 });
-
-
-// // Creating a GeoJSON layer with the retrieved data.
-// L.geoJSON(data, {
-
-//   // We turn each feature into a circleMarker on the map.
-  
-//   pointToLayer: function(feature, latlng) {
-//               console.log(data);
-//               return L.circleMarker(latlng);
-//           },
-//       }).addTo(map);
-//   });
-
-//   // Creating a GeoJSON layer with the retrieved data.
-//   L.geoJSON(data, {
-
-//     // We turn each feature into a circleMarker on the map.
-    
-//     pointToLayer: function(feature, latlng) {
-//                 console.log(data);
-//                 return L.circleMarker(latlng);
-//             },
-//           // We set the style for each circleMarker using our styleInfo function.
-//         style: styleInfo
-//         }).addTo(map);
-//     });
