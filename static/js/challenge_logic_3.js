87b25021a57eaa12952b22d3abc59f7b90201745
PDob02 <patrickdobry@gmail.com>
2022-03-19 19:03:41 -0500

    First push for the challenge

--- static/js/challenge_logic_3.js
+++ static/js/challenge_logic_3.js
@@ -14,12 +14,6 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     accessToken: API_KEY
 });
 
-// Create a base layer that holds both maps.
-let baseMaps = {
-  "Streets": streets,
-  "Satellite": satelliteStreets
-};
-
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [39.5, -98.5],
@@ -27,11 +21,43 @@ let map = L.map('mapid', {
   layers: [streets]
 });
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets
+};
+
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+// // Pass our map layers into our layers control and add the layers control to the map.
+// L.control.layers(baseMaps, overlays).addTo(map);
+
 
 // Accessing the airport GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/PDob02/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+// let torontoHoods = "https://raw.githubusercontent.com/PDob02/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 
 // // // Grabbing our GeoJSON data.
@@ -102,23 +128,75 @@ function getRadius(magnitude) {
 //       }).addTo(map);
 //   });
 
+// // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data, {
+//   // We turn each feature into a circleMarker on the map.
+//     pointToLayer: function(feature, latlng) {
+//         console.log(data);
+//         return L.circleMarker(latlng);
+//       },
+//   // We set the style for each circleMarker using our styleInfo function.
+//   style: styleInfo,
+//   // We create a popup for each circleMarker to display the magnitude and
+//   //  location of the earthquake after the marker has been created and styled.
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+//     }
+//   }).addTo(earthquakes);
+
+//     // earthquakes.addTo(map);
+
+// });
+
+
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data, {
-  // We turn each feature into a circleMarker on the map.
-  pointToLayer: function(feature, latlng) {
-      console.log(data);
-      return L.circleMarker(latlng);
-    },
-  // We set the style for each circleMarker using our styleInfo function.
-style: styleInfo,
-  // We create a popup for each circleMarker to display the magnitude and
-  //  location of the earthquake after the marker has been created and styled.
-  onEachFeature: function(feature, layer) {
-  layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-}
-}).addTo(map);
-});
+    // We turn each feature into a circleMarker on the map.
+      pointToLayer: function(feature, latlng) {
+          console.log(data);
+          return L.circleMarker(latlng);
+        },
+    // We set the style for each circleMarker using our styleInfo function.
+      style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+    }).addTo(earthquakes);
 
+      earthquakes.addTo(map);
+      // Create a legend control object.
+    let legend = L.control({
+      position: "bottomright"
+  });
+
+  // Then add all the details for the legend.
+    legend.onAdd = function(map) {
+      let div = L.DomUtil.create("div", "info legend");
+
+      const magnitudes = [0, 1, 2, 3, 4, 5];
+      const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+      ];
+
+  // Looping through our intervals to generate a label with a colored square for each interval.
+      for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+          "<i style='background: " + colors[i] + "'></i> " +
+          magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+      };
+
+    return div;
+  };
+    legend.addTo(map);
+});
 // // Grabbing our GeoJSON data.
 // d3.json(torontoHoods).then(function(data) {
 //   console.log(data);
--- static/js/major_eq_starter_logic.js
+++ static/js/major_eq_starter_logic.js
@@ -28,13 +28,14 @@ let baseMaps = {
   "Satellite": satelliteStreets
 };
 
-// 1. Add a 2nd layer group for the tectonic plate data.
+// 1. Add a 3rd layer group for the major earthquake data.
 let allEarthquakes = new L.LayerGroup();
 
 
-// 2. Add a reference to the tectonic plates group to the overlays object.
+// 2. Add a reference to the major earthquake group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes
+  "Earthquakes": allEarthquakes,
+
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -107,9 +108,33 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Then we add the earthquake layer to our map.
   allEarthquakes.addTo(map);
 
-  // Here we create a legend control object.
+// 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
+d3.json().then(function(data) {
+
+// 4. Use the same style as the earthquake data.
+
+
+// 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+
+
+// 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+
+
+// 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+// sets the style of the circle, and displays the magnitude and location of the earthquake
+//  after the marker has been created and styled.
+L.geoJson(, {
+    
+});
+// 8. Add the major earthquakes layer to the map.
+
+// 9. Close the braces and parentheses for the major earthquake data.
+});
+
+// Here we create a legend control object.
 let legend = L.control({
   position: "bottomright"
+
 });
 
 // Then add all the details for the legend
@@ -140,20 +165,8 @@ legend.onAdd = function() {
   legend.addTo(map);
 
 
-  // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
-
-    function styleInfo(feature) {
-      return {
-        opacity: 1,
-        fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
-        color: "#000000",
-        radius: getRadius(feature.properties.mag),
-        stroke: true,
-        weight: 0.5
-      };
-    }
+  // Use d3.json to make a call to get our Tectonic Plate geoJSON data.
+  d3.json().then(() {
     
   });
 });
